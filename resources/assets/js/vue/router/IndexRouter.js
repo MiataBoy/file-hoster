5e7bc0e0360fbdd1771827fa656070edfae2437a
Joshua Slui <joshua@dynamatrix.io>
2024-11-07 00:25:20 +0100

    Update route

--- resources/assets/js/vue/router/IndexRouter.js
+++ resources/assets/js/vue/router/IndexRouter.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     {
         path: "/",
-        name: "home",
+        name: "Home",
         component: () => import("../example.vue")
     }
 ];
