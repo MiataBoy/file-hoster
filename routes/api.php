82db07b75d8a1feb9ee5f75cfa3fe655eebfd6e4
Joshua Slui <joshua@dynamatrix.io>
2024-11-07 00:24:44 +0100

    Api routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/assets/upload', [\App\Http\Controllers\AssetController::class, 'assetUploader']);
+Route::get('/assets/get', [\App\Http\Controllers\AssetController::class, 'assetRetriever']);
